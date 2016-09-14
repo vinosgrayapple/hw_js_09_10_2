fc754cdec00f070bcb5b55effdecc89b2c38ece6
Sergey Komarichev <komarichev@gmail.com>
2016-09-15 00:50:30 +0300

    work with nav javascript

--- js/javascript.nav.js
+++ js/javascript.nav.js
@@ -1,3 +1,17 @@
-document.getElementsByClassName(".dropdown2").addEventListener("mouseenter", function(){
+ // = document.querySelectorAll(".dropdown2");
+var dropdown = document.querySelectorAll("li.dropdown2"),index;
+
+for (index = 0; index < dropdown.length; index++) {
+    dropdown2 = dropdown[index];
+    dropdown2.addEventListener('mouseenter', dropdownHandler);
+}
+
+function dropdownHandler(){
+	this.childNodes[2].style.display = "block";
+}
+
+    button.addEventListener('dblclick', doubleClickHandler);
+dropdown.addEventListener("click", function(){
 	this.style.display = "block";
+	// alert("button was clicked");
 });
